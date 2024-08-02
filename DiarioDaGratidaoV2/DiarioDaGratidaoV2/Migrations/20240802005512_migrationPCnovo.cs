486d57a2759bacfe40a36fdacb686a9b4c7afccc
Douglas Garcia Rangel <douglasgovmad@gmail.com>
2024-08-01 23:51:38 -0300

    colocando lista na pagina inicial

---
+++ DiarioDaGratidaoV2/DiarioDaGratidaoV2/Migrations/20240802005512_migrationPCnovo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiarioDaGratidaoV2.Migrations
+{
+    /// <inheritdoc />
+    public partial class migrationPCnovo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
